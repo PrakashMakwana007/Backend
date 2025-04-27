9ff133ea19e73b1c06507935027f82d673a9a253
PrakashMakwana007 <pasatvara7777@gmail.com>
2025-04-27 11:19:03 +0530

    fix: correct typo in mongoose connection variable name

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -3,7 +3,7 @@ import {DB_NAME} from "../constants.js";
 
 const connectDB = async () =>{
     try {
-       const connct = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`|| "mongodb+srv://prakash:prakash1234@makwana.c9ryz.mongodb.net/mydb")
+       const connct = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
        console.log(`MongoDB connected: ${connct.connection.host}`);       
     } catch (error) {
         console.log("mogodb connection error", error);
