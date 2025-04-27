2a96e21c8929cd79e46b1facb9892620ee90a9cc
PrakashMakwana007 <pasatvara7777@gmail.com>
2025-04-27 14:13:57 +0530

    fix: remove unnecessary slash in mongoose connection string

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -3,7 +3,7 @@ import {DB_NAME} from "../constants.js";
 
 const connectDB = async () =>{
     try {
-       const connct = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+       const connct = await mongoose.connect(`${process.env.MONGO_URI}${DB_NAME}`)
        console.log(`MongoDB connected: ${connct.connection.host}`);       
     } catch (error) {
         console.log("mogodb connection error", error);
