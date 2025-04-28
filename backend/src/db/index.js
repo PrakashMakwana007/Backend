350b031680471ee2cc192e3f6ec2a9b32f984e71
PrakashMakwana007 <pasatvara7777@gmail.com>
2025-04-28 11:27:40 +0530

    fix: remove commented-out MONGODB_URI validation and unused DB_NAME import

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,13 +1,9 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
     console.log("Connecting to MongoDB...", process.env.MONGODB_URI);
 
-    // if (!process.env.MONGODB_URI) {
-    //   throw new Error("MONGODB_URI is not defined in environment variables");
-    // }
     const connectionInstance = await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
