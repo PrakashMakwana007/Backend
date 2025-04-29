c8cbb9a6d1a027d4b620f50c0148c023d0ba9b07
PrakashMakwana007 <pasatvara7777@gmail.com>
2025-04-29 10:15:15 +0530

    fix: update CORS allowed origins for improved dynamic handling

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,9 @@ const app = express();
 
 const allowedOrigins = [
     'http://localhost:5173',
+    'http://localhost:5173',
+    'https://videotube-ivory.vercel.app',
+     /\.vercel\.app$/,
     'https://videotube-ivory.vercel.app',
     'https://videotube-git-main-makwana-prakashs-projects.vercel.app',
     'https://videotube-e294kjir7-makwana-prakashs-projects.vercel.app'
